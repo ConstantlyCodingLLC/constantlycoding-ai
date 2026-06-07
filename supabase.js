1b5bd948cced19de4098a0e02eaffe8aa2d5a1e5
ConstantlyCodingLLC <67442896+ConstantlyCodingLLC@users.noreply.github.com>
2026-06-07 01:42:07 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,13 +1,9 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-// 🔵 Your Supabase Project URL
 const SUPABASE_URL = "https://zorfqkepqjoapmikfrhk.supabase.co";
 
-// 🔵 Your ANON PUBLIC KEY (SAFE FOR FRONTEND)
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvcmZxa2VwcWpvYXBtaWtmcmhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODU2MzcsImV4cCI6MjA5NjM2MTYzN30.I6n2OiaZnwgov2UVCLDJ-RknI7Rfsj46RWme_2X4PHY";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvcmZxa2VwcWpvYXBtaWtmcmhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODU2MzcsImV4cCI6MjA5NjM2MTYzN30.I6n2OiaZnwgov2UVCLDJ-RknI7Rfsj46RWme_2X4PHY";
 
-export const supabase = createClient(
+window.supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
