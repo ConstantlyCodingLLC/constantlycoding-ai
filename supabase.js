dc26594d2399149c01a26e5f77c4e2328fce8542
ConstantlyCodingLLC <67442896+ConstantlyCodingLLC@users.noreply.github.com>
2026-06-07 22:49:47 -0500

    Refactor Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,9 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
 const SUPABASE_URL = "https://zorfqkepqjoapmikfrhk.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvcmZxa2VwcWpvYXBtaWtmcmhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODU2MzcsImV4cCI6MjA5NjM2MTYzN30.I6n2OiaZnwgov2UVCLDJ-RknI7Rfsj46RWme_2X4PHY";
 
-const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvcmZxa2VwcWpvYXBtaWtmcmhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODU2MzcsImV4cCI6MjA5NjM2MTYzN30.I6n2OiaZnwgov2UVCLDJ-RknI7Rfsj46RWme_2X4PHY";
-
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
