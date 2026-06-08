6b0a99be74ce924f88bf9b20fb547904eb55d630
ConstantlyCodingLLC <67442896+ConstantlyCodingLLC@users.noreply.github.com>
2026-06-07 23:02:36 -0500

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://zorfqkepqjoapmikfrhk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvcmZxa2VwcWpvYXBtaWtmcmhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODU2MzcsImV4cCI6MjA5NjM2MTYzN30.I6n2OiaZnwgov2UVCLDJ-RknI7Rfsj46RWme_2X4PHY";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
